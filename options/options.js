265aacb42254bd4944b4fa207646db04071dc28d
Crowdbotics <team@crowdbotics.com>
2023-05-30 13:27:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-breeze-41096",
+  url: "https://aged-breeze-41096.botics.co",
+  api: "https://aged-breeze-41096.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
